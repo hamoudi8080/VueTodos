a174690827f7fa08b4b68f3a9c898035175d9cb4
hamoudi8080 <hamoderaso@outlook.com>
2023-11-15 18:19:53 +0100

    intial

---
+++ src/main.js
@@ -0,0 +1,9 @@
+
+import router from "./router";
+import { createApp } from 'vue'
+import App from './App.vue'
+
+ 
+const app = createApp(App);
+app.mount("#app");
+app.use(router);
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Home View</h1>
+      <!-- Your template content goes here -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Your component options go here
+  }
+  </script>
